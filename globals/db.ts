240932112413f1e6d58342470cf018327c32dfaf
shoma-tanaka <72724492+shoma-tanaka-sg@users.noreply.github.com>
2024-01-04 17:28:52 +0900

    fix: remove server-only

--- globals/db.ts
+++ globals/db.ts
@@ -1,6 +1,5 @@
 // https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 import { PrismaClient } from '@prisma/client';
-import "server-only";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
